37097f227f886c70d4e6ca6c9fbbcd7ff12f75cc
AtifUsmani <73231217+AtifUsmani@users.noreply.github.com>
2021-01-02 00:31:06 +0530

    Made stable

--- assistant.py
+++ assistant.py
@@ -2,8 +2,6 @@ import pyttsx3
 import scraper
 import tasks
 import speech_recognition as sr
-import encrypter
-import cpu
 import music
 import os
 from cryptography.fernet import Fernet
@@ -41,42 +39,6 @@ def takeCommand():
         return "None"
     
     return query
-
-def assisstant(command):
-    if command == str("Open YT"):
-        scraper.youtube()
-        speak("Opening Youtube")
-    if command == str("Open Music app"):
-        scraper.YT_music()
-        speak("Opening YT Music")
-    if command == str("Open Github"):
-        scraper.Github()
-        speak("Opening Github")
-    if command == str("Enter a task = str"):
-        task_input = input(str("Enter a new task: "))
-        tasks.text(task_input)
-        speak("Changes saved")
-    if command == str("Enter a task = int"):
-        task_input = input("Enter a new task: ")
-        task_input_float = int(float(task_input))
-        tasks.text(task_input_float)
-        speak("Changes saved")
-    if command == str("Encrypt"):
-        encrypter_input = input(str("Enter a string to encrypt: "))
-        encrypter.encrypt(bytes(encrypter_input, encoding='utf-8'))
-        # encrypter_input = message
-    if command == str("Decrypt"):
-        decrypter_input = input(str("Enter to decrypt: "))
-        encrypter.decrypt(bytes(decrypter_input, encoding='utf-8'))
-    if command == str("Play some music"):
-        music_choice = input(str("Should I open YT Music or Play locally?"))
-        if music_choice == str("Play from YT Music"):
-            scraper.YT_music()
-        elif music_choice == str("Play Locally"):
-                # pick an external mp3 player you have
-                music.play_music("/home/atif/Documents/MyProjects/Assistant/Music/Post Malone - Better Now.mp3")
-# command_input = input("Enter command: ")
-# assisstant(command_input)
 
 if __name__ == "__main__":
     clear = lambda: os.system('cls')
